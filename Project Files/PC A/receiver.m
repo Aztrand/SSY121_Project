0d3ed5b89810614c1ea42a23be3fdc867dca7fa1
Aztrand <oscar_aspestrand@hotmail.com>
2016-09-22 15:07:46 +0200

    .
    .

--- Project Files/PC A/receiver.m
+++ Project Files/PC A/receiver.m
@@ -1,13 +1,10 @@
 function [pack, psd, const, eyed] = receiver(tout,fc)
 % Complete the function and remove the following lines
-disp('Complete the receiver') 
-pack = []; psd = [];  const=[]; eyed = [];
 
-for aq=-3:2:3
-    for ai=-3:2:3
-        if 
-    end
-end
 
+
+
+%disp('Complete the receiver') 
+%pack = []; psd = [];  const=[]; eyed = [];
 
 end
