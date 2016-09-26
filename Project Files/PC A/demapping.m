f9de28b4d441495b7e21de7918f08135c71aba85
Aztrand <oscar_aspestrand@hotmail.com>
2016-09-26 19:11:13 +0200

    Changes

--- Project Files/PC A/demapping.m
+++ Project Files/PC A/demapping.m
@@ -1,24 +1,21 @@
-clear all;
-clc; clf;
-
-
+function [ output_args ] = demapping( input_args )
+%DEMAPPING Summary of this function goes here
+%   Detailed explanation goes here
 
 %Constellation
 IQ=([0 2 10 8;  1 3 11 9  ...
 ;5 7 15 13;  4 6 14 12]);
-
 iq=([-3 -3; -1 -1; 1 1; 3 3]);
-
 %Q=1; I=3;
+s=[];
 for i=1:length(iq)
         Qnorm = (iq(i,1)+3)/2+1;
         Inorm = (iq(i,2)+3)/2+1;
         mess=IQ(Qnorm,Inorm);
         messB(i,:)=de2bi(mess,4);
+        s = [s,messB(i,:)];
 end
 
-messB
-%mess=IQ(Qnorm,Inorm)
-%messB=de2bi(mess)
-%mess=bi2de(messB)
+
+end
 
--- Project Files/PC A/testreceiver.m
+++ Project Files/PC A/testreceiver.m
@@ -10,14 +10,16 @@ IQ=([0 2 10 8;  1 3 11 9  ...
 iq=([-3 -3; -1 -1; 1 1; 3 3]);
 
 %Q=1; I=3;
+s=[];
 for i=1:length(iq)
         Qnorm = (iq(i,1)+3)/2+1;
         Inorm = (iq(i,2)+3)/2+1;
         mess=IQ(Qnorm,Inorm);
         messB(i,:)=de2bi(mess,4);
+        s = [s,messB(i,:)]
 end
 
-messB
+messB;
 %mess=IQ(Qnorm,Inorm)
 %messB=de2bi(mess)
 %mess=bi2de(messB)
