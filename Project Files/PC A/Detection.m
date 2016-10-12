25d583a67b29208115b53ab5b5147330cf6c6d1b
RunViktor <runnby@student.chalmers.se>
2016-10-12 17:06:14 +0200

    change detect

--- Project Files/PC A/Detection.m
+++ Project Files/PC A/Detection.m
@@ -1,8 +1,8 @@
 detect=0;
 sound_old =[];
-
+fs = 8000;
 while(detect~=1)
-    sound_in = wavrecord(N,fs); %N should be number of samples for barker code + a small margin
+    sound_in = wavrecord((13+1)*fsfd,fs); %N should be number of samples for barker code + a small margin
     
     % Do crosscorr of sound_in with reference barker
     sound = [sound_old sound_in];
@@ -14,6 +14,7 @@ while(detect~=1)
     crosscorrthreshold = ;
     if (r>crosscorrthreshold)
         detect = 1;
+        N2 = 13*fsfd+432/2*fsfd+length(guard);
         signal = wavrecord(N2,fs); %N should be guard+signal
     end
     sound_old = sound_in;
