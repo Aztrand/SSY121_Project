566208d35631878de219eddbfcdceb9807fb1189
RunViktor <runnby@student.chalmers.se>
2016-10-13 14:58:06 +0200

    Removed 2s

--- Project Files/PC A/pbTObb.m
+++ Project Files/PC A/pbTObb.m
@@ -2,8 +2,8 @@ function [ y1 ] = pbTObb( y,fc,Tsamp )
 %PBTOBB Summary of this function goes here
 %   y is the received signal
 n = (0:length(y)-1); %changed to '
-y_real = sqrt(2)*y.*2.*cos(2*pi*fc*Tsamp.*n');
-y_image = sqrt(2)*y.*-2.*sin(2*pi*fc*Tsamp.*n');
+y_real = sqrt(2)*y.*cos(2*pi*fc*Tsamp.*n');
+y_image = sqrt(2)*y.*sin(2*pi*fc*Tsamp.*n');
 y1 = y_real+1i.*y_image;
 %N_y1 = max(1024,length(y1)); 
 %y1_fft = abs(fftshift(fft(y1,N_y1)));
